20ec534b4f1cb4c683611c84cdbb8b205995ca30
LabEG <labeg@mail.ru>
2015-04-03 00:37:25 +0300

    Create editor.

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*global ace, LabEG*/
 
+(function(){
+    "use strict";
+    
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.getSession().setMode("ace/mode/html");
+    
+    var xmldocformatter = new LabEG.Lib.XMLDOCFormatter();
+    
+    document.getElementById("format").addEventListener("click", function(){
+        editor.setValue(xmldocformatter.format(editor.getValue()));
+        xmldocformatter.clear();
+    });
+        
+}());
